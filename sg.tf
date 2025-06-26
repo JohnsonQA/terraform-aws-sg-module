9c803e8b5a10c59ae7e3eda54196367560d29552
JohnsonQA <Johnson.kudumula09@gmail.com>
2025-06-26 20:16:21 +0530

    vpc module

--- sg.tf
+++ sg.tf
@@ -4,7 +4,16 @@ resource "aws_security_group" "main" {
 
     name = var.sg_name
     description = var.sg_description
-    vpc_id = var.vpc_id
+    vpc_id = var.vpc_id                  # Attaching vpc ID to the sg group
+
+    #add rule to allow egress or outbound traffic to connect internet. so it's always open 
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
     tags = merge(
         var.sg_tags,
